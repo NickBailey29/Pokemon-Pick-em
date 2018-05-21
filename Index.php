b771ba8e184ee409bc93c402b3803c3d2fa823b6
NickBailey29 <bainica@dunwoody.edu>
2018-05-21 06:17:25 -0500

    Add files via upload

---
+++ Index.php
@@ -0,0 +1,60 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    
+    
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    
+	<style>
+	.bg-dark {
+	  background-color: #563d7c !important;
+	}
+	</style>
+
+    <title>CWEB 2018 Spring Semester projects </title>
+  </head>
+  <header>
+	<nav class="navbar navbar-expand-md navbar-dark bg-dark flex-md-row">
+	<ul class="navbar-nav bd-navbar-nav flex-row">
+  	<li class="nav-item">
+	  <a class="nav-link active" href="Index.php">Home</a>
+	</li>
+    <li class="nav-item">
+	  <a class="nav-link" href="hw1/indexHW1.php">HW 1</a>
+	</li>
+	<li class="nav-item">
+		<a class="nav-link" href="lab1/indexLab1.php">Lab 1</a>
+	</li>
+	<li class="nav-item">
+		<a class="nav-link" href="lab2/indexLab2.php">Lab 2</a>
+	</li>
+	<li class="nav-item">
+		<a class="nav-link" href="hw2/indexHW2.php">HW 2</a>
+	</li>
+ 	<li class="nav-item">
+		<a class="nav-link" href="slc/index.php">SLC</a>
+	</li>
+	 <li class="nav-item">
+		<a class="nav-link" href="capstone/Index.php">Capstone</a>
+	</li>
+	</ul>
+
+   </nav>
+   </header
+   <body>
+   <main role="main" class="container">
+     <div class="jumbotron">
+    	<h1>CWEB 2018 Spring Semester projects</h1>
+	 </div>
+   </main>
+    </body>
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+
+</html>
